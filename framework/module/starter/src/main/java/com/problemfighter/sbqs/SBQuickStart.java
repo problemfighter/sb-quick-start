be7e35b8e6016d49272f70ae05b86483e0d8be41
Touhid Mia <hmtm.cse@gmail.com>
2020-07-08 09:06:14 +0600

    Added Global Dependencies

---
+++ framework/module/starter/src/main/java/com/problemfighter/sbqs/SBQuickStart.java
@@ -0,0 +1,14 @@
+package com.problemfighter.sbqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication()
+public class SBQuickStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.problemfighter.sbqs.SBQuickStart.class, args);
+	}
+
+}
