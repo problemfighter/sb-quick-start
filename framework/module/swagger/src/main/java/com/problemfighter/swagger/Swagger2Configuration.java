0585e848d2e364d59e597491cbfc05c7d4de90a8
Touhid Mia <hmtm.cse@gmail.com>
2020-07-13 16:15:43 +0600

    Spring Boot Swagger

---
+++ framework/module/swagger/src/main/java/com/problemfighter/swagger/Swagger2Configuration.java
@@ -0,0 +1,17 @@
+package com.problemfighter.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebMvc
+public class Swagger2Configuration {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+}
