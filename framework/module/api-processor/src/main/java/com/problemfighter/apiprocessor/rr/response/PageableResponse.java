b6165e33e0761b61dc468c49c715991f4f100101
Touhid Mia <hmtm.cse@gmail.com>
2020-07-22 23:45:46 +0600

    Spring Boot Swagger Pageable

--- framework/module/api-processor/src/main/java/com/problemfighter/apiprocessor/rr/response/PageableResponse.java
+++ framework/module/api-processor/src/main/java/com/problemfighter/apiprocessor/rr/response/PageableResponse.java
@@ -2,7 +2,10 @@ package com.problemfighter.apiprocessor.rr.response;
 
 import io.swagger.annotations.ApiModel;
 
+import java.util.List;
+
 @ApiModel(value = "pageable", description = "status success response")
-public class PageableResponse<T> extends DetailsResponse<T> {
+public class PageableResponse<T> extends ErrorAndBaseData {
+    public List<T> data;
     public PaginationData pagination;
 }
