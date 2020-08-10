6dbf9ca1ab85989daaf747b5273d649ce8f3a258
Touhid Mia <hmtm.cse@gmail.com>
2020-08-10 17:32:00 +0600

    Mapping API

--- framework/module/api-processor/src/main/java/com/problemfighter/apiprocessor/rr/ReqProcessor.java
+++ framework/module/api-processor/src/main/java/com/problemfighter/apiprocessor/rr/ReqProcessor.java
@@ -8,6 +8,14 @@ import java.util.LinkedHashMap;
 
 public class ReqProcessor extends ObjectCopier {
 
+    public <D> D copySrcToDst(Object source, D destination) {
+        try {
+            return copy(source, destination);
+        } catch (ObjectCopierException e) {
+            ApiProcessorException.otherError(e.getMessage());
+        }
+        return null;
+    }
 
     public <D> D copySrcToDst(Object source, Class<D> destination) {
         try {
@@ -30,6 +38,11 @@ public class ReqProcessor extends ObjectCopier {
         return copySrcToDst(source, destination);
     }
 
+    public <D> D copySrcToDstValidate(Object source, D destination) {
+        dataValidate(source);
+        return copySrcToDst(source, destination);
+    }
+
     // Quick Access
     public static ReqProcessor instance() {
         return new ReqProcessor();
@@ -39,7 +52,15 @@ public class ReqProcessor extends ObjectCopier {
         return instance().copySrcToDst(source, destination);
     }
 
+    public static <D> D copyOnly(Object source, D destination) throws ApiProcessorException {
+        return instance().copySrcToDst(source, destination);
+    }
+
     public static <D> D process(Object source, Class<D> destination) {
+        return instance().copySrcToDstValidate(source, destination);
+    }
+
+    public static <D> D process(Object source, D destination) {
         return instance().copySrcToDstValidate(source, destination);
     }
 
